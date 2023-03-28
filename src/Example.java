54135640f5b0dc5c48227a8e46463e536a7cecfe
Solomon <solomonabebe23@gmail.com>
2023-03-28 05:11:50 +0300

    data typesexamples

---
+++ src/Example.java
@@ -0,0 +1,42 @@
+public class Example {
+    public static void main(String[] args){
+        byte numberByte=127;
+        short numberShort=32767;
+        int numberInt=2147483647;
+        float numberFloat=21474836400000000000.2f;
+        float area=64.5f;
+
+        char letterV='S';
+        String firstName="Solomon";
+        String fName="Hana";
+        String name=fName;
+
+        boolean isItTrue=true;
+        boolean isItFalse=false;
+
+        int number1=8;
+        int number2=5;
+
+        boolean hasCompletedFinalProject=false;
+        boolean hasCompleteGrade=true;
+        boolean hasConductIssue=false;
+
+        System.out.println(hasCompletedFinalProject && hasCompleteGrade || hasConductIssue);
+
+       // System.out.println(isItTrue &&isItFalse );
+      //  System.out.println(isItTrue || isItFalse );
+
+
+        /*System.out.println(numberByte);
+        System.out.println(numberShort);
+        System.out.println(numberInt);
+        System.out.println(numberFloat);
+        System.out.println(isItTrue);
+        System.out.println(letterV);
+        System.out.println(name);
+                 */
+
+
+    }
+
+}
