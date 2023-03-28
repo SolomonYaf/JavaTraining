22b102b4212fd1b35e1470190e855fe8774fce6e
Solomon <solomonabebe23@gmail.com>
2023-03-28 05:23:02 +0300

    data type casting

---
+++ src/TypeCasting.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Please insert random number between 1 &10");
+//        int userInput= input.nextInt();
+//
+//        int computerNum= (byte)(Math.random()*10);
+//        System.out.println(computerNum);
+//
+//        String guessNum= (userInput==computerNum)?" Congratulations! you are lucky":" Thank you, Try again";
+//        System.out.println(guessNum);
+//
+        Random random = new Random();
+       // random.nextInt(4,8);
+        System.out.println( random.nextInt(4,8));
+
+
+
+    }
+}
