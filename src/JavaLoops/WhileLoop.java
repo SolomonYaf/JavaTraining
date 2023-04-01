29959f76f32484a2ba00b0a88aaa7009c245b96a
Solomon <solomonabebe23@gmail.com>
2023-04-01 07:36:12 +0300

    while loop

---
+++ src/JavaLoops/WhileLoop.java
@@ -0,0 +1,17 @@
+package JavaLoops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i=1;
+
+        while (i<=10){
+            System.out.println(i);
+            i+=2;
+            int  j=1;
+            while (j<=5){
+                System.out.println("Java is fun");
+                j++;
+            }
+        }
+    }
+}
