2f7ade94fd8b9759b029f61fa3dd3f505423d2c4
Solomon <solomonabebe23@gmail.com>
2023-04-01 07:24:15 +0300

    for loop and nested for loop

--- src/JavaLoops/ForLoop.java
+++ src/JavaLoops/ForLoop.java
@@ -4,6 +4,12 @@ public class ForLoop {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i+=2) {
             System.out.println(i);
+
+            int n = 5;
+            // for loop
+            for (int j = 1; j <= n; ++j) {
+                System.out.println("Java is fun");
+            }
         }
     }
 }
