cf7c0ed6141dbbb29501c0c3559303e2c82fdb8a
Solomon <solomonabebe23@gmail.com>
2023-04-08 03:45:47 +0300

    Arrays on Vowels

---
+++ src/Arrays/Vowels.java
@@ -0,0 +1,10 @@
+package Arrays;
+import java.util.Arrays;
+
+public class Vowels {
+    public static void main(String[] args) {
+        char[] vowels = {'a', 'o', 'u', 'i', 'e'};
+        Arrays.sort(vowels);
+        System.out.println(Arrays.toString(vowels));
+    }
+}
