4df1dd68f1a7ca184c7951a6924143b59ceb81ff
Solomon <solomonabebe23@gmail.com>
2023-04-09 09:53:09 +0300

    Tax Calculation using while and if else

--- src/JavaLoops/WhileLoopIfElseTax.java
+++ src/JavaLoops/WhileLoopIfElseTax.java
@@ -11,6 +11,7 @@ public class WhileLoopIfElseTax {
 
         double incomeTax;
         double pensionTax;
+        double netPay;
 //        System.out.println(" Do you want to calculate your salary?");
 //        String answer = scanner.next();
 //        if (answer.equalsIgnoreCase("Yes")){
@@ -18,23 +19,30 @@ public class WhileLoopIfElseTax {
             if (salary>=60001) {
                 incomeTax = salary * 0.3;
                 pensionTax = salary * .07;
+                netPay=salary-incomeTax-pensionTax;
+
 
                 System.out.println(" Income Tax: "+incomeTax + "  &  Pension Tax: "+pensionTax);
+                System.out.println(" Your net pay is: "+ netPay);
                 break;
             }
                 else if (salary>=30001){
 
                 incomeTax = salary * 0.15;
                 pensionTax = salary * .05;
+                netPay=salary-incomeTax-pensionTax;
 
                 System.out.println(" Income Tax: "+incomeTax + "  &  Pension Tax: "+pensionTax);
+                System.out.println(" Your net pay is: "+ netPay);
                 break;
                 }
             else if (salary>5000){
 
                 incomeTax = salary * 0.1;
                 pensionTax = salary * .03;
+                netPay=salary-incomeTax-pensionTax;
                 System.out.println(" Income Tax: "+incomeTax + " & Pension Tax: "+pensionTax);
+                System.out.println(" Your net pay is: "+ netPay);
                 break;
             }
 
